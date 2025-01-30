0de46ae8b453d3aa8881012c3b5f9f2a28bd66f8
Harrison Fornasier <harrisonfornasier@gmail.com>
2025-01-30 12:35:11 +0000

    Add Prisma query for getting posts

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -3,8 +3,13 @@ CREATE TABLE "posts" (
     "id" SERIAL NOT NULL,
     "title" VARCHAR NOT NULL,
     "content" TEXT NOT NULL,
-    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
-
+    "created_at" TIMESTAMPTZ (6) DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
 );
 
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR(255),
+    "password" VARCHAR(255) NOT NULL,
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
